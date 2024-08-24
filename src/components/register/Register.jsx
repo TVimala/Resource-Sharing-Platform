e360b8d40aeff3069f8a7f89b62e09a802f5fd93
TVimala <vimalareddytummuru@gmail.com>
2024-08-24 16:52:37 +0530

    add:login-Register

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import './Login.css'
 import {useForm} from 'react-hook-form'
-import { Link } from 'react-router-dom';
-function Login() {
+import './Register.css'
+function Register() {
     let { register, handleSubmit, formState: { errors } } = useForm();
   return (
     <div>
         <div className=''>
-        <h1 className='text-center'>Login</h1>
+        <h1 className='text-center'>Register</h1>
         <div className='row'>
         <div className='col-11 col-sm-10 col-md-6 mx-auto'>
-            <form action="" className=' login rounded p-5'>
+            <form action="" className='register rounded p-5'>
             <div className='mb-3'>
          <label htmlFor="username" className='form-label'>Username</label>
          <input type="text" id='username'{...register('username',{required:true})} className='form-control' />
@@ -28,10 +27,19 @@ function Login() {
           </div> */}
          {errors.password?.type==='required' && <p className='text-danger'>Password Required</p>}
         </div>
-        <div className='d-flex justify-content-evenly'>
-            <button className=' btn btn-dark'>Login</button>
+        <div className='mb-3'>
+         <label htmlFor="email" className='form-label'>Email</label>
+         <input type="email" id='email'{...register('email',{required:true})} className='form-control' />
+          {errors.email?.type==='required' && <p className='text-danger'>Email required</p>}
         </div>
-        {/* <p className='text-center'>New User? <Link to="/register">Register</Link></p> */}
+        <div className='mb-3'>
+         <label htmlFor="number" className='form-label'>Phone Number</label>
+         <input type="number" id='number'{...register('number',{required:true})} className='form-control' />
+          {errors.number?.type==='required' && <p className='text-danger'>PhoneNumber required</p>}
+        </div>
+        <div className='d-flex justify-content-evenly'>
+            <button className=' btn btn-dark'>Register</button>
+        </div>
         </form>
         </div> 
         </div>
@@ -39,4 +47,5 @@ function Login() {
     </div>
   )
 }
-export default Login
+
+export default Register
