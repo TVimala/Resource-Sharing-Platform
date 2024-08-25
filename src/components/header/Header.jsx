f4baa8494bcff53ebe96456b6714607e4d72f496
TVimala <vimalareddytummuru@gmail.com>
2024-08-25 21:32:38 +0530

    add:homechanges

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,15 +6,14 @@ function Header() {
   return (
     <div className='header'>
       <div className='logo'>
-        MyApp
+        <h3>ResoLink</h3>
       </div>
       <ul className='nav justify-content-end'>
           <li className='nav-item'>
-            <Link to="login" className='nav-link text-white'>Login</Link>
+            <Link to="login" className='nav-link text-white'><h5>Login</h5></Link>
           </li>
       </ul>
     </div>
   )
 }
-
 export default Header
