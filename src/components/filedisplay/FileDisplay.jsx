5f918d8f0f5b5f58eac499a9c416ae12ff407638
TVimala <vimalareddytummuru@gmail.com>
2024-09-02 23:12:40 +0530

    add:files

--- src/components/filedisplay/FileDisplay.jsx
+++ src/components/filedisplay/FileDisplay.jsx
@@ -3,10 +3,15 @@ import { FaFileAlt } from 'react-icons/fa';
 function FileDisplay({ driveLink, fileName }) {
   return (
     <div className="d-flex align-items-center my-2">
-      <a href='https://drive.google.com/file/d/1rs0VN3ZIIWwPddtdQiZSRjSY3saDaSov/view?usp=drivesdk' target="_blank" rel="noopener noreferrer" className="d-flex align-items-center text-decoration-none text-dark">
-        <FaFileAlt className="me-2" style={{ fontSize: '24px' }} />
+<iframe 
+  src="https://drive.google.com/file/d/1rs0VN3ZIIWwPddtdQiZSRjSY3saDaSov/view?usp=drivesdk" 
+  frameBorder="0" 
+  scrolling="no" 
+></iframe>
+      {/* <a href='https://drive.google.com/file/d/1rs0VN3ZIIWwPddtdQiZSRjSY3saDaSov/view?usp=drivesdk' target="_blank" rel="noopener noreferrer" className="d-flex align-items-center text-decoration-none text-dark"> */}
+        {/* <FaFileAlt className="me-2" style={{ fontSize: '24px' }} />
         <span className="fs-5">{fileName || 'Click to view the file'}</span>
-      </a>
+      </a> */}
     </div>
   );
 }
