ad3d7c411dd4e43faef5d7781fb2143d9e3f7262
tej <reddytej15@gmail.com>
2024-09-21 22:18:28 +0530

    graph

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Uploadgraph from '../uploadGraph/Uploadgraph'
+import { userLoginContext } from '../../contexts/userLoginContext';
+import { useContext } from 'react';
+
 function Profile(){
+  let {currentUser}=useContext(userLoginContext)
+
   return (
    <div>
     Profile
+    <Uploadgraph username={currentUser.username} />
   </div> 
   )
 }
