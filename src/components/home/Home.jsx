435b10343a488f0133ef6b655cb1a0cbdf142036
Mithil <realmithil@gmail.com>
2024-10-02 12:32:00 +0530

    MODIFIED HOMEPAGE

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import CourseCards from '../courseCards/CourseCards'
-import cardData from '../cardData'
-import './Home.css'
+import './Home.css';
 
 function Home() {
   return (
     <div className='home-container text-center'>
+      {/* First image design: Header and button */}
       <h1 className='resource p-3'>Resource-Sharing-Platform</h1>
-     <button className="start btn fs-4  " >START</button>
-     <div className='courseimg m-5 d-flex'>
-       <CourseCards cardData={cardData}/>
-       </div>
+
+      <button className="start btn fs-4 bg-white">START</button>
+
+      {/* Second image design: Courses section */}
+      {/* <div className='courseimg m-5 d-flex'>
+        <CourseCards cardData={cardData} />
+      </div> */}
+
+      {/* Third image design: About section */}
+      <div className='about-section d-flex'>
+        <div className='about-text'>
+          <h2>About</h2>
+          <p>
+          ResoLink is a platform where students can upload and find academic resources with ease. Use tags to categorize and search for notes, study guides, and more. Join ResoLink to access a wide range of shared materials and boost your learning.          </p>
         </div>
-  )
+        <img src='bookshelf.jpg' alt='Bookshelf' className='about-image' />
+      </div>
+    </div>
+  );
 }
-export default Home
+
+export default Home;
+
